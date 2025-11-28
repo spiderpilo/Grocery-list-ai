c67da3cdac027076639d8b92e1bc263f90320c98
Piolo Patag <134972709+spiderpilo@users.noreply.github.com>
2025-11-28 14:46:34 -0800

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge } = require('electron');
-const Store = require('electron-store');
-
-const store = new Store();
-
-contextBridge.exposeInMainWorld('api', {
-  getList: () => store.get('groceryList', []),
-  setList: (list) => store.set('groceryList', list)
-});
